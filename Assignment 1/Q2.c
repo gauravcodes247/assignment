f347b27625c5e26fe887fcfec86c0499391597d0
Gaurav Janghu <24csu063@ncuindia.edu>
2024-11-17 21:30:02 +0530

    Add files via upload

---
+++ Assignment 1/Q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int findHCF(int a, int b) 
+{
+    while (b != 0) 
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int main() 
+{   
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    int hcf = findHCF(num1, num2);
+    printf("The HCF of %d and %d is: %d\n", num1, num2, hcf);
+    
+    return 0;
+}
